9449087890e1b53e633c43853071ba330483beeb
Ishaan M <55643770+im-ishaanm@users.noreply.github.com>
2019-10-22 20:24:31 +0530

    Initial commit
    Added all the files.

---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EditTodo() {
+    return (
+        <div>
+            <p>Welcome to Edit Todos Component.</p>
+        </div>
+    );
+}
+
+
+export default EditTodo;
